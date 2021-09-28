36da966d11e6bb59195fbd735a2436bf9b1ed8cb
Lashonda Miller <lashondamiller007@gmail.com>
2021-09-28 13:56:13 -0400

    initial commit

---
+++ GeometricShapes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GeometricShapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
